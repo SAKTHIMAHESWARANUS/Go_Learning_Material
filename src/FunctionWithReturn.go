0d7c26e1cdc88a8f47b76497bc06ac3e7ec5bed3
Sakthimaheswaran-u-s <sakthi@guvi.in>
2023-08-14 18:36:35 +0530

    added other fnc

---
+++ src/FunctionWithReturn.go
@@ -0,0 +1,12 @@
+package main
+import ("fmt")
+
+func add(a int , b int ) (res int) { // named return values
+	res = a+b;
+	return;
+}
+
+func main()  {
+	sum_value := add(23,45); // storing return value in a variable
+	fmt.Println(sum_value);
+}
--- src/Range.go
+++ src/Range.go
@@ -3,7 +3,7 @@ import ("fmt")
 
 func main()  {
 	letters := [4]string{"a","b","c","d"};
-	for idx, val := range letters{ // if dont want idx then for _,val 
+	for idx, val := range letters{ // if we dont want idx then for _,val 
 		fmt.Printf("%v %v\n",idx,val);       // if we dont want val then for idx,_
 	}
 }
