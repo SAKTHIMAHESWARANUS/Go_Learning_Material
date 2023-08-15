9051034afa3c1aaf908ebd0fdb6a74bc8c3c2271
Sakthimaheswaran-u-s <sakthi@guvi.in>
2023-08-15 11:10:25 +0530

    changes at folders

--- src/Range.go
+++ src/Range.go
@@ -4,7 +4,8 @@ import ("fmt")
 func main()  {
 	letters := [4]string{"a","b","c","d"};
 	for idx, val := range letters{ // if we dont want idx then for _,val 
-		fmt.Printf("%v %v\n",idx,val);       // if we dont want val then for idx,_
+		// fmt.Printf("%v %v\n",idx,val);       // if we dont want val then for idx,_
+		fmt.Println(val,idx);
 	}
 }
 	
